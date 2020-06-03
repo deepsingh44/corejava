c0359abf027e66675416b875417d9d7b2a9bf33b
Deep Singh <Deep Singh@DESKTOP-T0PSQD7>
2020-06-03 18:21:36 +0530

    First Commit.

--- FirstExample/src/HelloWorld.java
+++ FirstExample/src/HelloWorld.java
@@ -1,9 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		// this is a statement to print HelloWorld
-		// Where System is a predefine class and out is reference variable and
-		// println is a method 
-		System.out.println("Hello World");
+
 	}
 }
